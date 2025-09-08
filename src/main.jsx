57aa9d299417525080de226455d092dcd10fe743
zvio-coder <zvio@hotmail.co.uk>
2025-09-08 11:11:25 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,7 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
-
-// If your file is named src/App.jsx use this:
 import App from './App.jsx'
-
-// If your file is currently named src/app.jsx (lowercase), use this line instead of the one above:
-// import App from './app.jsx'
+import './styles.css'
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
